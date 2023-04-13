f6b8b0916e3ea639d1de73d5aeff8e46492b76cb
0404ecol <oliver.eckerstrom@gmail.com>
2023-04-13 12:38:39 +0200

    logbok och problem

--- HockeyManager/Controllers/GameController.cs
+++ HockeyManager/Controllers/GameController.cs
@@ -1,36 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
 
-namespace HockeyManager.Controllers
-{
-    public class GameController : Controller
-    {
-        public IActionResult Home()
-        {
-            return View();
-        }
-        public IActionResult Team()
-        {
-            return View();
-        }
-        public IActionResult Player()
-        {
-            return View();
-        }
-        public IActionResult Market()
-        {
-            return View();
-        }
-        public IActionResult Leaderboard()
-        {
-            return View();
-        }
-        public IActionResult Statistics()
-        {
-            return View();
-        }
-        public IActionResult History()
-        {
-            return View();
-        }
-    }
-}
+//namespace HockeyManager.Controllers
+//{
+//    public class GameController : Controller
+//    {
+//        public IActionResult Home()
+//        {
+//            return View();
+//        }
+//        public IActionResult Team()
+//        {
+//            return View();
+//        }
+//        public IActionResult Player()
+//        {
+//            return View();
+//        }
+//        public IActionResult Market()
+//        {
+//            return View();
+//        }
+//        public IActionResult Leaderboard()
+//        {
+//            return View();
+//        }
+//        public IActionResult Statistics()
+//        {
+//            return View();
+//        }
+//        public IActionResult History()
+//        {
+//            return View();
+//        }
+//    }
+//}
--- HockeyManager/Models/User.cs
+++ HockeyManager/Models/User.cs
@@ -1,9 +1,9 @@
-namespace HockeyManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+//namespace HockeyManager.Models
+//{
+//    public class ErrorViewModel
+//    {
+//        public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
+//        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+//    }
+//}
