40bbb1a003fe9179a395641c454a41fee0040cd6
0404ecol <oliver.eckerstrom@gmail.com>
2023-05-16 14:33:04 +0200

    burrrrrrrr

--- HockeyManager/wwwroot/js/inside.js
+++ HockeyManager/wwwroot/js/inside.js
@@ -6,129 +6,44 @@ burgerElement.addEventListener('click', () => {
     navElement.classList.toggle('active');
 });
 
-function sortTable(attribute) {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementsByTagName("table")[0];
-    switching = true;
+// Pagination
+function paginateTable(pageNumber) {
+    var table, rowsPerPage, rows, totalPages, i;
 
-    while (switching) {
-        switching = false;
-        rows = table.rows;
+    table = document.getElementById("playerTable");
+    rowsPerPage = 30;
+    rows = table.getElementsByTagName("tr");
+    totalPages = Math.ceil((rows.length - 1) / rowsPerPage);
 
-        for (i = 1; i < rows.length - 1; i++) {
-            shouldSwitch = false;
-            x = rows[i].getElementsByTagName("td")[getAttributeIndex(attribute)].innerHTML;
-            y = rows[i + 1].getElementsByTagName("td")[getAttributeIndex(attribute)].innerHTML;
-
-
-            if (attribute === "number" || attribute === "power" || attribute === "cost") {
-                // Convert the values to numbers for numerical comparisons
-                x = parseFloat(x);
-                y = parseFloat(y);
-            }
-
-            if (x > y) {
-                shouldSwitch = true;
-                break;
-            }
-
-            if (i > rowsPerPage * currentPage) {
-                console.log("break")
-                break;
-            }
-        }
-
-        if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
+    for (i = 1; i < rows.length; i++) {
+        if (i > (pageNumber * rowsPerPage) || i <= ((pageNumber - 1) * rowsPerPage)) {
+            rows[i].style.display = "none";
+        } else {
+            rows[i].style.display = "";
         }
     }
+
+    generatePaginationButtons(pageNumber, totalPages);
 }
 
-function getAttributeIndex(attribute) {
-    switch (attribute) {
-        case "name":
-            return 0;
-        case "number":
-            return 1;
-        case "role":
-            return 2;
-        case "power":
-            return 3;
-        case "cost":
-            return 4;
-        default:
-            return 0;
-    }
-}
-// Global variables
-var currentPage = 1;
-var rowsPerPage = 100; // Number of rows to display per page
+function generatePaginationButtons(currentPage, totalPages) {
+    var paginationDiv = document.getElementById("pagination");
+    paginationDiv.innerHTML = "";
 
-// Function to display a specific page of rows
-function displayRows(page) {
-    var table = document.getElementById('playerTable');
-    var rows = table.rows;
-    var totalRows = rows.length - 1; // Exclude the header row
-
-    // Calculate the start and end index of the rows to display
-    var startIndex = (page - 1) * rowsPerPage + 1; // Exclude the header row
-    var endIndex = Math.min(startIndex + rowsPerPage, totalRows + 1);
-
-    // Hide all rows initially
-    for (var i = 1; i < rows.length; i++) {
-        rows[i].style.display = 'none';
-    }
-
-    // Display the selected rows for the current page
-    for (var j = startIndex; j < endIndex; j++) {
-        rows[j].style.display = '';
-    }
-
-    // Update pagination buttons
-    updatePagination(page, Math.ceil(totalRows / rowsPerPage));
-}
-
-// Function to update the pagination buttons
-function updatePagination(currentPage, totalPages) {
-    var pagination = document.getElementById('pagination');
-    pagination.innerHTML = '';
-
-    // Create and append previous button
-    var prevButton = document.createElement('button');
-    prevButton.innerText = 'Previous';
-    prevButton.disabled = currentPage === 1;
-    prevButton.addEventListener('click', function() {
-        if (currentPage > 1) {
-            displayRows(currentPage - 1);
-        }
-    });
-    pagination.appendChild(prevButton);
-
-    // Create and append page buttons
     for (var i = 1; i <= totalPages; i++) {
-        var pageButton = document.createElement('button');
-        pageButton.innerText = i;
-        pageButton.disabled = currentPage === i;
-        pageButton.addEventListener('click', function(page) {
-            return function() {
-                displayRows(page);
-            };
-        }(i));
-        pagination.appendChild(pageButton);
-    }
+        var button = document.createElement("button");
+        button.innerHTML = i;
+        button.addEventListener("click", function () {
+            paginateTable(parseInt(this.innerHTML));
+        });
 
-    // Create and append next button
-    var nextButton = document.createElement('button');
-    nextButton.innerText = 'Next';
-    nextButton.disabled = currentPage === totalPages;
-    nextButton.addEventListener('click', function() {
-        if (currentPage < totalPages) {
-            displayRows(currentPage + 1);
+        if (i === currentPage) {
+            button.classList.add("active");
         }
-    });
-    pagination.appendChild(nextButton);
+
+        paginationDiv.appendChild(button);
+    }
 }
 
-// Initial setup
-displayRows(currentPage);
+// Initial pagination setup
+paginateTable(1);
